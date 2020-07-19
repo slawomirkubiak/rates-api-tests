08c48dbafb87cee5fc4627a8d85ec6e1d5595686
slawomirkubiak <68349115+slawomirkubiak@users.noreply.github.com>
2020-07-19 21:05:37 +0200

    refactor: move url building to separate step, setup connection settings in @before, add tags

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -1,34 +1,59 @@
 package steps;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class StepDefinitions {
 
+
+    private RequestSpecification request;
+    private String fullUrl;
     private Response response;
+    private static int TIMEOUT = 400;
 
     private String apiUrl = "https://api.ratesapi.io/api/";
 
+    @Before
+    public void buildApiUrl() {
+
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    RestAssured.config = RestAssured
+        .config()
+        .httpClient(HttpClientConfig.httpClientConfig()
+            .setParam("http.connection.timeout", TIMEOUT)
+            .setParam("http.socket.timeout", TIMEOUT)
+            .setParam("http.connection-manager.timeout", TIMEOUT));
+    
+    request = given().when();
+    }
+
     @When("I put {string} in URL")
-    public void iPutStringInURL(String urlString) {
-        response = given().log().all().when().get(apiUrl + urlString);
+    public void iPutUrlStringInURL(String urlString) {
+        urlString = (urlString.equals("(no string)")) ? "" : urlString; //replace null value description from feature file with real null
+        fullUrl = apiUrl + urlString;
     }
 
     @Then("status code {int} is returned")
     public void statusCodeStatusCodeIsReturned(int statusCode) {
         response.then().statusCode(statusCode);
-
     }
 
     @And("error message {string} is returned")
     public void errorMessageErrorMessageIsReturned(String errorMessage) {
         assertEquals(response.path("error"), errorMessage);
+    }
 
+    @And("I hit Rates API")
+    public void hitRatesAPI() {
+        response = request.get(fullUrl);
     }
 }
--- src/test/resources/invalidUrl.feature
+++ src/test/resources/invalidUrl.feature
@@ -1,12 +1,14 @@
 Feature: Invalid URL syntax
 
+  @invalid-url @negative-tests
   Scenario Outline: I hit Rates API with incorrect endpoint URL
-    When I put <urlString> in URL
+    When I put "<urlString>" in URL
+    And I hit Rates API
     Then status code <statusCode> is returned
-    And error message <errorMessage> is returned
+    And error message "<errorMessage>" is returned
 
     Examples:
-      | urlString     | statusCode | errorMessage                                               |
-      | ""            | 400        | "time data 'api' does not match format '%Y-%m-%d'"         |
-      | "2019-Dec-29" | 400        | "time data '2019-Dec-29' does not match format '%Y-%m-%d'" |
-      | "sampleDate"  | 400        | "time data 'sampleDate' does not match format '%Y-%m-%d'"  |
+      | urlString   | statusCode | errorMessage                                             |
+      | (no string) | 400        | time data 'api' does not match format '%Y-%m-%d'         |
+      | 2019-Dec-29 | 400        | time data '2019-Dec-29' does not match format '%Y-%m-%d' |
+      | sampleDate  | 400        | time data 'sampleDate' does not match format '%Y-%m-%d'  |
