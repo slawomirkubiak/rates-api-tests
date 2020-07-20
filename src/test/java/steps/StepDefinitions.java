e0710151f2b82c63fc0ac563eb2397ec3ecd5e7f
slawomirkubiak <68349115+slawomirkubiak@users.noreply.github.com>
2020-07-20 21:09:22 +0200

    test: add positive scenario for "latest" endpoint and valid "base" parameter

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -7,19 +7,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.config.HttpClientConfig;
+import io.restassured.path.json.config.JsonPathConfig;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.JsonConfig.jsonConfig;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class StepDefinitions {
 
@@ -29,11 +30,21 @@ public class StepDefinitions {
     private Response response;
     private static int TIMEOUT = 2000;
 
+    private static String DEFAULT_BASE = "EUR"; //default value in API
+    private String base = DEFAULT_BASE;
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
     private String apiUrl = "https://api.ratesapi.io/api";
-    private static List<String> ALL_CURRENCIES = Arrays.asList("GBP", "HKD", "IDR", "ILS", "DKK", "INR", "CHF",
+    private LinkedList<String> ALL_CURRENCIES = new LinkedList<>(Arrays.asList("GBP", "HKD", "IDR", "ILS", "DKK", "INR", "CHF",
             "MXN", "CZK", "SGD", "THB", "HRK", "MYR", "NOK", "CNY", "BGN", "PHP", "SEK", "PLN", "ZAR",
-            "CAD", "ISK", "BRL", "RON", "NZD", "TRY", "JPY", "RUB", "KRW", "USD", "HUF", "AUD");
-    private List<String> expectedCurrencies;
+            "CAD", "ISK", "BRL", "RON", "NZD", "TRY", "JPY", "RUB", "KRW", "USD", "HUF", "AUD", "EUR"));
     private static String CURRENT_OR_PREVIOUS_WORKING_DAY_STRING = "(current date or previous working day)";
     private static LocalDate CURRENT_DATE = LocalDate.now();
     private static LocalDate PREVIOUS_WORKING_DAY = TestHelpers.getPreviousWorkingDay(LocalDate.now());
@@ -47,7 +58,8 @@ public class StepDefinitions {
                 .httpClient(HttpClientConfig.httpClientConfig()
                         .setParam("http.connection.timeout", TIMEOUT)
                         .setParam("http.socket.timeout", TIMEOUT)
-                        .setParam("http.connection-manager.timeout", TIMEOUT));
+                        .setParam("http.connection-manager.timeout", TIMEOUT))
+                .jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL));
 
         request = given()
                 .urlEncodingEnabled(false) //turn off replacing comma to %2C etc.
@@ -98,7 +110,7 @@ public class StepDefinitions {
             ));
 
         } else {
-            assertEquals(response.path("error"), errorMessage);
+            assertEquals(errorMessage, response.path("error"));
         }
     }
 
@@ -109,18 +121,26 @@ public class StepDefinitions {
 
     @And("base currency {string} is returned")
     public void baseCurrencyIsReturned(String base) {
-        assertEquals(response.path("base"), base);
+        assertEquals(base, response.path("base"));
     }
 
     @And("numerical values of exchange rate are returned for {string}")
     public void numericalValuesOfExchangeRateAreReturnedFor(String rates) {
-        if (rates.equals("(all currencies)")) {
-            expectedCurrencies = ALL_CURRENCIES; // replace ("all currencies") description with it names list
-        } else {
-            expectedCurrencies = Arrays.asList(rates.split("\\s*,\\s*"));
+        List<String> expectedCurrencies;
+
+        switch (rates) {
+            case "(all currencies)":
+                expectedCurrencies = ALL_CURRENCIES;
+                expectedCurrencies.remove(getBase()); // remove base currency from the currencies list
+                break;
+            case "(all currencies including base currency)":
+                expectedCurrencies = ALL_CURRENCIES;
+                break;
+            default:
+                expectedCurrencies = Arrays.asList(rates.split("\\s*,\\s*")); // load expected currencies from feature file
         }
 
-        Map<String, Float> ratesFromApi = response.then().extract().response().path("rates"); // extract all rates from API response, check if they have numerical values
+        Map<String, BigDecimal> ratesFromApi = response.then().extract().response().path("rates"); // extract all rates from API response, check if they have numerical values
         List<String> currenciesFromApi = new ArrayList(ratesFromApi.keySet()); // move just currencies to separate list
 
         TestHelpers.compareListsIfEquals(expectedCurrencies, currenciesFromApi);
@@ -138,4 +158,27 @@ public class StepDefinitions {
         }
     }
 
+    @And("I set base parameter to {string}")
+    public void iSetBaseParameterTo(String baseCurrencyInParameter) {
+
+        switch (baseCurrencyInParameter) {
+            case "(empty parameter)":
+                break; //skip the logic if "symbols" parameter shouldn't be set
+            case "(no symbol parameter)":
+                request = request.param("base", ""); // replace empty symbol value description from feature file with real empty value
+            default:
+                request = request.param("base", baseCurrencyInParameter);
+                setBase(baseCurrencyInParameter);
+        }
+
+    }
+
+    @And("exchange rate for {string} currency equals {string}")
+    public void exchangeRateForCurrencyEquals(String baseCurrencyInResponse, String expectedBaseCurrencyFxRate) {
+        if (expectedBaseCurrencyFxRate.equals("(not present)")) {
+            assertNull(response.path("rates." + getBase()));
+        } else {
+            assertEquals(new BigDecimal(expectedBaseCurrencyFxRate), response.path("rates." + baseCurrencyInResponse));
+        }
+    }
 }
--- src/test/resources/latestEndpoint.feature
+++ src/test/resources/latestEndpoint.feature
@@ -1,24 +1,41 @@
 Feature: Latest FX rates endpoint
 
-  @latest @positive-tests
+  @latest @positive-tests @symbols
   Scenario Outline: I hit latest FX rates endpoint of Rates API with valid symbol parameter
     When I set endpoint to latest
     And I set symbols parameter to "<symbols>"
     And I hit Rates API
     Then status code <statusCode> is returned
-    And base currency "<baseCurrency>" is returned
+    And base currency "<baseCurrencyInResponse>" is returned
     And numerical values of exchange rate are returned for "<rates>"
     And date "<date>" is returned
 
     Examples:
-      | symbols               | statusCode | baseCurrency | rates               | date                                   |
-      | USD                   | 200        | EUR          | USD                 | (current date or previous working day) |
-      | USD,GBP,CHF,CZK,SEK   | 200        | EUR          | USD,GBP,CHF,CZK,SEK | (current date or previous working day) |
-      | (no symbol parameter) | 200        | EUR          | (all currencies)    | (current date or previous working day) |
-      | (empty parameter)     | 200        | EUR          | (all currencies)    | (current date or previous working day) |
+      | symbols               | statusCode | baseCurrencyInResponse | rates               | date                                   |
+      | USD                   | 200        | EUR                    | USD                 | (current date or previous working day) |
+      | USD,GBP,CHF,CZK,SEK   | 200        | EUR                    | USD,GBP,CHF,CZK,SEK | (current date or previous working day) |
+      | (no symbol parameter) | 200        | EUR                    | (all currencies)    | (current date or previous working day) |
+      | (empty parameter)     | 200        | EUR                    | (all currencies)    | (current date or previous working day) |
+
+  @latest @positive-tests @base
+  Scenario Outline: I hit latest FX rates endpoint of Rates API with valid base parameter
+    When I set endpoint to latest
+    And I set base parameter to "<baseCurrencyInParameter>"
+    And I hit Rates API
+    Then status code <statusCode> is returned
+    And base currency "<baseCurrencyInResponse>" is returned
+    And exchange rate for "<baseCurrencyInResponse>" currency equals "<rateForBaseCurrency>"
+    And numerical values of exchange rate are returned for "<rates>"
+    And date "<date>" is returned
+
+    Examples:
+      | baseCurrencyInParameter | statusCode | baseCurrencyInResponse | rates                                    | rateForBaseCurrency | date                                   |
+      | USD                     | 200        | USD                    | (all currencies including base currency) | 1.0                 | (current date or previous working day) |
+      | GBP                     | 200        | GBP                    | (all currencies including base currency) | 1.0                 | (current date or previous working day) |
+      | (empty parameter)       | 200        | EUR                    | (all currencies)                         | (not present)       | (current date or previous working day) |
 
 
-  @latest @negative-tests
+  @latest @negative-tests @symbols
   Scenario Outline: I hit latest FX rates endpoint of Rates API with invalid symbol parameter
     When I set endpoint to latest
     And I set symbols parameter to "<symbols>"
